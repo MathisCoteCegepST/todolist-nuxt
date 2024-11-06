bff521942ab44e409a213fc8f233d151fe099b52
BenocxX <mathis.cote@outlook.com>
2024-11-06 09:46:35 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Counter: {{ count }}</h1>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const count = ref(0);
+</script>
